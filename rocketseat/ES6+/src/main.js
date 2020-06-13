f480109a7f22185fe443b99eccc881231ce52f50
alanams97 <alan_ams97@gmail.com>
2020-06-13 16:35:14 -0300

    feat/ Loading

--- rocketseat/ES6+/src/main.js
+++ rocketseat/ES6+/src/main.js
@@ -15,6 +15,18 @@ class App {
         this.formEL.onsubmit = event => this.addRepository(event)
     }
 
+    setLoading(loading = true) {
+        if (loading === true) {
+            let loadingEl = document.createElement('span')
+            loadingEl.appendChild(document.createTextNode('Carregando...'))
+            loadingEl.setAttribute('id','loading')
+
+            this.formEL.appendChild(loadingEl)
+        } else {
+            document.getElementById('loading').remove()
+        }
+    }
+
     async addRepository(event) {
         event.preventDefault();
 
@@ -22,25 +34,31 @@ class App {
         if (repoInput.length === 0)
             return
 
-        const response = await api.get(`/repos/${repoInput}`)
-        const { name, description, html_url, owner: { avatar_url } } = response.data
+            this.setLoading()
 
-        console.log(response.data)
+        try {
+            const response = await api.get(`/repos/${repoInput}`)
+            const { name, description, html_url, owner: { avatar_url } } = response.data
 
-        this.repositories.push({
-            name,
-            description,
-            avatar_url,
-            html_url
-        })
+            this.repositories.push({
+                name,
+                description,
+                avatar_url,
+                html_url
+            })
 
-        this.inputEL.value = ""
+            this.inputEL.value = ""
 
-        this.render()
+            this.render()
+        } catch(error) {
+            alert('O repositório não existe!')
+        }
+
+        this.setLoading(false)
     }
 
     render() {
-        this.listEL.innerHTML= ""
+        this.listEL.innerHTML = ""
 
         this.repositories.forEach(repo => {
             let imgEl = document.createElement('img')
